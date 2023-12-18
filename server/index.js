81a6374f295ccedfb7969548b8a59683a3232b19
Josh Genereux <jgenereux@stratacache.com>
2023-12-18 11:10:40 -0500

    added api key into.env file

--- server/index.js
+++ server/index.js
@@ -6,15 +6,16 @@ const path = require('path');
 const ctrl = require('./controller');
 const { default: axios } = require('axios');
 const PORT = process.env.SERVER_PORT || 3001;
+const { WATCH_BASE_API_KEY: key } = process.env;
 
-const key = 'B4PMCSMEXeGo1c0Lpk5HmEQw2bKhSqaIwzhuJ9cy';
+const URL = 'https://api.watchbase.com/v1/';
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/api/all', async (req, res) => {
   try {
-    const url = `https://api.watchbase.com/v1/brands?format=json&key=${key}`;
+    const url = `${URL}brands?format=json&key=${key}`;
     const response = await axios.get(url);
     res.json(response.data);
   } catch (error) {
@@ -25,7 +26,7 @@ app.get('/api/all', async (req, res) => {
 app.post(`/api/brand`, async (req, res) => {
   try {
     const id = req?.body?.[0]?.id;
-    const url = `https://api.watchbase.com/v1/families?brand-id=${id}&format=json&key=${key}`;
+    const url = `${URL}families?brand-id=${id}&format=json&key=${key}`;
     const response = await axios.get(url);
     res.json(response.data);
   } catch (error) {
@@ -35,9 +36,17 @@ app.post(`/api/brand`, async (req, res) => {
 
 app.get('/api/search', async (req, res) => {
   try {
-    const url = `https://api.watchbase.com/v1/watch?id=17289&format=json&key=${key}`;
+    const url = `${URL}watch?id=17289&format=json&key=${key}`;
     const response = await axios.get(url);
     res.json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/api/watches', async (req, res) => {
+  try {
+    // const url =
   } catch (error) {
     console.log(error);
   }
